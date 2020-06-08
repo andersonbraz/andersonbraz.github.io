537ed5738b2fe1588e0316f5e87a4dfc7610e288
Anderson Braz <braz_anderson@msn.com>
2020-06-08 18:56:03 -0300

    Up Files

--- RocketSeat/Skylab/Javascript/main-advanced.js
+++ RocketSeat/Skylab/Javascript/main-advanced.js
@@ -1,36 +1,42 @@
+
+var listElement = document.querySelector(".card .list-group");
+
 function buscar() {
+
   var inputElement = document.querySelector("#username");
-  var listElement = document.querySelector(".card .list-group");
   var username = inputElement.value;
 
   if (username.length > 0) {
-    listElement.innerHTML = "";
+
+    listElement.innerHTML = "<li class='list-group-item'>Carregando...</li>";
 
     axios
       .get("https://api.github.com/users/" + username + "/repos")
       .then(function (response) {
         
         var repos = response.data;
+        listElement.innerHTML = "";
 
         for (repo of repos) {
-          
-            var itemElement = document.createElement("li");
-
-            itemElement.setAttribute("class", "list-group-item");
-            itemElement.innerText = repo.name;
-            listElement.appendChild(itemElement);
-
+          appendItem(repo.name);
         }
 
       })
       .catch(function (error) {
-
-            var itemElement = document.createElement("li");
-
-            itemElement.setAttribute("class", "list-group-item");
-            itemElement.innerText = "Not Found";
-            listElement.appendChild(itemElement);
-
+        listElement.innerHTML = "";
+        listElement.innerHTML = "<li class='list-group-item'>Not Found</li>";
       });
   }
 }
+
+
+function appendItem(item){
+
+  var itemElement = document.createElement("li");
+
+  itemElement.setAttribute("class", "list-group-item");
+  itemElement.innerText = repo.name;
+  listElement.appendChild(itemElement);
+
+}
+
