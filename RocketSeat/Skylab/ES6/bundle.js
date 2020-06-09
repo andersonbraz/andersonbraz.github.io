9566e5a64bd8b2920ac30d4200d854bddf22b816
Anderson Braz <braz_anderson@msn.com>
2020-06-09 15:31:33 -0300

    Up Files

--- RocketSeat/Skylab/ES6/bundle.js
+++ RocketSeat/Skylab/ES6/bundle.js
@@ -1,22 +1,58 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+var Usuario = function Usuario() {
+  _classCallCheck(this, Usuario);
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+  this.email = null;
+  this.senha = null;
+  this.admin = false;
+};
 
-var TodoList = /*#__PURE__*/function () {
-  function TodoList() {
-    _classCallCheck(this, TodoList);
+var Admin = /*#__PURE__*/function (_Usuario) {
+  _inherits(Admin, _Usuario);
 
-    this.todos = [];
+  var _super = _createSuper(Admin);
+
+  function Admin() {
+    var _this;
+
+    _classCallCheck(this, Admin);
+
+    _this = _super.call(this);
+    _this.admin = true;
+    return _this;
   }
 
-  _createClass(TodoList, [{
-    key: "addTodo",
-    value: function addTodo() {}
-  }]);
+  return Admin;
+}(Usuario);
+/* Example Create User and Admin */
 
-  return TodoList;
-}();
+
+document.querySelector("#button-test").onclick = function () {
+  var usuario = new Usuario();
+  usuario.email = 'contato@andersonbraz.com';
+  usuario.senha = 'Yml$Cs93GHt89!';
+  console.log(usuario);
+  var admin = new Admin();
+  admin.email = 'admin@andersonbraz.com';
+  admin.senha = 'Net@CfTG#h67$';
+  console.log(admin);
+};
--- RocketSeat/Skylab/ES6/main.js
+++ RocketSeat/Skylab/ES6/main.js
@@ -1,11 +1,37 @@
-class TodoList{
-    
+class Usuario{
+
     constructor(){
-        this.todos = [];
+        this.email = null;
+        this.senha = null;
+        this.admin = false;
     }
 
-    addTodo(){
+}
 
+class Admin extends Usuario{
+
+    constructor(){
+        super();
+        this.admin = true;
     }
+
+}
+
+/* Example Create User and Admin */ 
+
+document.querySelector("#button-test").onclick = function(){
+
+    var usuario =  new Usuario();
+    usuario.email = 'contato@andersonbraz.com';
+    usuario.senha = 'Yml$Cs93GHt89!';
+    console.log(usuario);
+
+    var admin =  new Admin();
+    admin.email = 'admin@andersonbraz.com';
+    admin.senha = 'Net@CfTG#h67$';
+    console.log(admin);
+
+
+    
 
 }
